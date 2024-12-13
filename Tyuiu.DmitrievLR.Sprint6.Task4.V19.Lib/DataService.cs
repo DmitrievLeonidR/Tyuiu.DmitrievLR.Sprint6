fc1b4197a9f7b786860f25c9fa4ce4fc55403c6e
DmitrievLeonidR <Tyuiu.DmitrievLR2@outlook.com>
2024-12-13 20:22:34 +0500

    Task4

---
+++ Tyuiu.DmitrievLR.Sprint6.Task4.V19.Lib/DataService.cs
@@ -0,0 +1,37 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.DmitrievLR.Sprint6.Task4.V19.Lib
+{
+    public class DataService : ISprint6Task4V19
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            int arrayLength = stopValue - startValue + 1;
+
+            double[] functionValues = new double[arrayLength];
+
+            for (int i = 0; i < arrayLength; i++)
+            {
+
+                int x = startValue + i;
+
+                functionValues[i] = CalculateFunctionValue(x);
+            }
+
+            return functionValues;
+        }
+
+        private double CalculateFunctionValue(int x)
+        {
+
+            if (Math.Sin(x) - 2 == 0)
+            {
+                return 0;
+            }
+
+            double functionValue = (5 * x + 2.5) / (Math.Sin(x) - 2) + 2;
+
+            return Math.Round(functionValue, 2);
+        }
+    }
+}
